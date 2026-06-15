b863384e962c69c351fdf747aa13bae8569ec07e
Fenaz Mohamed <lafeerfenazmo@gmail.com>
2026-06-15 15:52:27 +0530

    IP

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-
-const API_URL = 'https://zksacisrzh.execute-api.eu-north-1.amazonaws.com/';
+const API_URL = 'https://docnexus-py.onrender.com/';
+//const API_URL = 'https://zksacisrzh.execute-api.eu-north-1.amazonaws.com/';
 
 const api = axios.create({
   baseURL: API_URL,
